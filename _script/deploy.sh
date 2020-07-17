a3aac695587cf215cce47fe4eaed12ae43b3776d
tanish-kr <tora.1986.tatsu@gmail.com>
2020-07-17 20:19:40 +0900

    added deploy script

---
+++ _script/deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash -e
+
+deploy_dir="_site"
+html_files=`find $deploy_dir -name '*.html' -type f`
+s3_bucket="s3://www.tanish-kr.com"
+
+for file in $html_files; do
+  if [[ ! $file =~ 'index.html' ]]; then
+    reprace_name=`echo $file | sed -r 's/\.html//'`
+    mv $file $reprace_name
+  fi
+done
+
+aws s3 sync $deploy_dir $s3_bucket --exclude "*.md" --exclude "*.js" --exclude "*.css" --exclude "*.xml" --exclude ".DS_Store" --acl public-read --content-type "text/html"
+aws s3 sync $deploy_dir $s3_bucket --exclude "*.md" --include "*.js" --include "*.css" --include "*.xml" --exclude ".DS_Store" --acl public-read
